73629842d90e70aa0992932df59e84ca7543866d
stanislaw-am-i <stanislav.rusakov@succraft.com>
2023-04-17 23:53:29 +0200

    Refactor MessageRepository

--- src/main/java/org/receiver/messages/MessageRepository.java
+++ src/main/java/org/receiver/messages/MessageRepository.java
@@ -22,107 +22,54 @@ public class MessageRepository {
 
         for (Element rawMessage : parsedData) {
             if ( rawMessage.classNames().contains("default") ) {
-                String textMessage = rawMessage.getElementsByClass("text").text();
-                Boolean isMessageTooLong = !textMessage.isBlank() && textMessage.length() > 500;
-
-                if ( !isMessageTooLong ) {
-                    Message message = new Message();
-                    message.setMessageId( rawMessage.attr("id") );
-
-                    String photoPath = rawMessage.getElementsByClass("photo_wrap").attr("href");
-                    if ( !photoPath.isBlank() ) {
-                        Media photo = new Media();
-                        photo.setPathToFile( "src/main/resources/ChatExport_2023-04-09/" + photoPath );
-                        photo.setMessageId(message.getMessageId());
-
-                        message.addMediaFileToRepository(photo);
-                    }
-
-                    Element reply = rawMessage.getElementsByClass("reply_to").first();
-                    if ( reply != null ) {
-                        String replyHref = reply.getElementsByTag("a").first().attr("href");
-
-                        Pattern pattern = Pattern.compile("(message.*)");
-                        Matcher matcher = pattern.matcher(replyHref);
-                        if ( matcher.find() ) {
-                            message.setParentMessageId( matcher.group(1) );
-                        }
-                    }
-
-                    if ( !textMessage.isBlank() ) {
-                        message.setTextMessage(textMessage);
-                    }
-
-                    Boolean isMessageEmpty = message.isMessageEmpty();
-                    if ( !isMessageEmpty ) {
-                        addMessage(message);
-                    }
-                } else {
-                    splitLongMessage(rawMessage, textMessage);
-                }
+                processMessages(rawMessage);
             }
         }
     }
 
-    public Map<String, Message> getMessages() {
-        return messages;
-    }
+    private void processMessages(Element rawMessage) {
+        Message message = new Message();
+        message.setMessageId( rawMessage.attr("id") );
 
-    public Set<String> getMessagesIds() {
-        return messagesIds;
-    }
-
-    public List<Media> getMediaStorage() {
-        return mediaStorage;
-    }
-
-    public void addMessage(Message message) {
-        messages.put(message.getMessageId(), message);
-        messagesIds.add(message.getMessageId());
-        mediaStorage.addAll(message.getMediaRepository());
-        System.out.println( message.getMessageId() );
-        countMessages++;
-    }
-
-    public void splitLongMessage(Element rawMessage, String messageText) {
-        Message mainMessage = new Message();
-        mainMessage.setMessageId( rawMessage.attr("id") );
+        Element reply = rawMessage.getElementsByClass("reply_to").first();
+        String replyTo = getParentMessageId(reply);
+        if ( replyTo!= null && !replyTo.isBlank() ) {
+            message.setParentMessageId(replyTo);
+        }
 
         String photoPath = rawMessage.getElementsByClass("photo_wrap").attr("href");
         if ( !photoPath.isBlank() ) {
             Media photo = new Media();
             photo.setPathToFile( "src/main/resources/ChatExport_2023-04-09/" + photoPath );
-            photo.setMessageId(mainMessage.getMessageId());
+            photo.setMessageId(message.getMessageId());
 
-            mainMessage.addMediaFileToRepository(photo);
+            message.addMediaFileToRepository(photo);
         }
 
-        Element reply = rawMessage.getElementsByClass("reply_to").first();
-        if ( reply != null ) {
-            String replyHref = reply.getElementsByTag("a").first().attr("href");
-
-            Pattern pattern = Pattern.compile("(message.*)");
-            Matcher matcher = pattern.matcher(replyHref);
-            if ( matcher.find() ) {
-                mainMessage.setParentMessageId( matcher.group(1) );
-            }
+        String textMessage = rawMessage.getElementsByClass("text").text();
+        if (textMessage.length() > 500) {
+            splitLongMessage(message, textMessage);
+        } else {
+            message.setTextMessage(textMessage);
+            addMessage(message);
         }
+    }
 
+    private void splitLongMessage(Message mainMessage, String messageText) {
         String[] parts = messageText.split("(?<=[.!?]\\s)");
         List<String> chunks = new ArrayList<>();
         int maxStatusLength = 489;
 
-        String currentChunk = "";
+        StringBuilder currentChunk = new StringBuilder();
         for (String sentence : parts) {
             if (currentChunk.length() + sentence.length() > maxStatusLength) {
-                chunks.add(currentChunk);
-                currentChunk = sentence;
+                chunks.add(currentChunk.toString());
+                currentChunk = new StringBuilder(sentence);
             } else {
-                currentChunk += " " + sentence;
+                currentChunk.append(" ").append(sentence);
             }
         }
-
-        chunks.add(currentChunk);
+        chunks.add(currentChunk.toString());
 
         int totalCount = chunks.size();
         String parentMessageId = mainMessage.getMessageId();
@@ -143,6 +90,83 @@ public class MessageRepository {
                 addMessage(mainMessage);
             }
         }
+    }
+
+    private String getParentMessageId(Element reply) {
+        if (reply == null) {
+            return null;
+        }
+
+        String replyHref = reply.getElementsByTag("a").first().attr("href");
+        Pattern pattern = Pattern.compile("(message.*)");
+        Matcher matcher = pattern.matcher(replyHref);
+
+        return matcher.find() ? matcher.group(1) : null;
+    }
+
+
+
+
+
+    /*public void processMessages(Element rawMessage) {
+        String textMessage = rawMessage.getElementsByClass("text").text();
+        Boolean isMessageTooLong = !textMessage.isBlank() && textMessage.length() > 500;
+
+        if ( !isMessageTooLong ) {
+            Message message = new Message();
+            message.setMessageId( rawMessage.attr("id") );
+
+            String photoPath = rawMessage.getElementsByClass("photo_wrap").attr("href");
+            if ( !photoPath.isBlank() ) {
+                Media photo = new Media();
+                photo.setPathToFile( "src/main/resources/ChatExport_2023-04-09/" + photoPath );
+                photo.setMessageId(message.getMessageId());
+
+                message.addMediaFileToRepository(photo);
+            }
+
+            Element reply = rawMessage.getElementsByClass("reply_to").first();
+            if ( reply != null ) {
+                String replyHref = reply.getElementsByTag("a").first().attr("href");
+
+                Pattern pattern = Pattern.compile("(message.*)");
+                Matcher matcher = pattern.matcher(replyHref);
+                if ( matcher.find() ) {
+                    message.setParentMessageId( matcher.group(1) );
+                }
+            }
+
+            if ( !textMessage.isBlank() ) {
+                message.setTextMessage(textMessage);
+            }
+
+            Boolean isMessageEmpty = message.isMessageEmpty();
+            if ( !isMessageEmpty ) {
+                addMessage(message);
+            }
+        } else {
+            splitLongMessage(rawMessage, textMessage);
+        }
+    }*/
+
+    public Map<String, Message> getMessages() {
+        return messages;
+    }
+
+    public Set<String> getMessagesIds() {
+        return messagesIds;
+    }
+
+    public List<Media> getMediaStorage() {
+        return mediaStorage;
+    }
+
+    public void addMessage(Message message) {
+        messages.put(message.getMessageId(), message);
+        messagesIds.add(message.getMessageId());
+        mediaStorage.addAll(message.getMediaRepository());
+        System.out.println( message.getMessageId() );
+        countMessages++;
     }
 
     public int getCountMessages() {
